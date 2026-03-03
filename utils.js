7b2325e5dc01581f512d692f387af106f3c5dc67
isuretti <i.suretti@gmail.com>
2026-03-03 17:49:51 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-03T14:49:42.640Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-03T14:49:50.707Z
+Commit: Fix CSS styling bug on mobile devices

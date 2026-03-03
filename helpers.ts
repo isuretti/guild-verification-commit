72261ad954328917492ca56dbbe799ae7ddd7174
isuretti <i.suretti@gmail.com>
2026-03-03 17:50:04 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-03T14:50:00.924Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-03T14:50:03.802Z
+Commit: Correct typo in configuration file

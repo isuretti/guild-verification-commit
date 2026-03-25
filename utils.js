1299d42e1a86b00d23c6d49d8191d954b871eec4
isuretti <i.suretti@gmail.com>
2026-03-25 23:04:17 +0300

    Add multi-language support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-25T20:04:14.341Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-25T20:04:17.227Z
+Commit: Add multi-language support

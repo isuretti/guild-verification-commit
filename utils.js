b41215f1fad4b6439cc91dc622323f744ecc4783
isuretti <i.suretti@gmail.com>
2026-03-25 23:04:14 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-25T20:04:09.195Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-25T20:04:14.341Z
+Commit: Resolve null pointer exception in user service

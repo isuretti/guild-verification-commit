725ec05eeeb250f0f4ac16f661ac720f910f0994
isuretti <i.suretti@gmail.com>
2026-03-25 23:04:36 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-25T20:04:17.227Z
-Commit: Add multi-language support
+Timestamp: 2026-03-25T20:04:35.594Z
+Commit: Patch security vulnerability in authentication

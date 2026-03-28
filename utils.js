9e92bd7a23b0739cc28a41015c0ff7f9c2d3f0e5
isuretti <i.suretti@gmail.com>
2026-03-28 21:52:19 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-25T20:04:36.820Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-28T18:52:18.380Z
+Commit: Fix memory leak in event handlers

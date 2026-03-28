e7d01a06595e12b3ce8503757fdb444decda19e5
isuretti <i.suretti@gmail.com>
2026-03-28 21:52:26 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-28T18:52:21.685Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-28T18:52:25.481Z
+Commit: Resolve null pointer exception in user service

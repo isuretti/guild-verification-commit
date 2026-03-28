93713b468dc36f78c5436b2645b4da75cc37a2f2
isuretti <i.suretti@gmail.com>
2026-03-28 21:52:47 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-28T18:52:26.777Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-28T18:52:46.894Z
+Commit: Fix CSS styling bug on mobile devices

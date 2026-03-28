da0dc0698b2b534b17cdf10786aa013fb6266381
isuretti <i.suretti@gmail.com>
2026-03-28 21:51:52 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-25T20:04:29.731Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-28T18:51:52.304Z
+Commit: Fix CSS styling bug on mobile devices

17a3ae1e6291eae8d6d0f102014f0eca30d36ff4
isuretti <i.suretti@gmail.com>
2026-03-28 21:51:59 +0300

    Add export functionality for reports

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-28T18:51:52.304Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-28T18:51:59.033Z
+Commit: Add export functionality for reports

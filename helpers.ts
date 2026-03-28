ae06916f16d8f580bca929ebb13128513505d0ba
isuretti <i.suretti@gmail.com>
2026-03-28 21:52:17 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-28T18:52:03.159Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-28T18:52:16.629Z
+Commit: Resolve null pointer exception in user service

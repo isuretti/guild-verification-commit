af96e699c363944f665190f7703c2491681aa65b
isuretti <i.suretti@gmail.com>
2026-03-28 21:51:58 +0300

    Fix database connection timeout

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-25T20:04:32.465Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-28T18:51:57.421Z
+Commit: Fix database connection timeout
